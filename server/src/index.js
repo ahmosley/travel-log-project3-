605354f377a26a679e028cd7e1914ac9fb01e153
Anna Mosley <annahmosley@gmail.com>
2021-03-27 09:56:12 -0400

    set up server

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,28 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
+require('dotenv').config();
+const mongoose = require('mongoose');
 const middlewares = require('./middleware');
+const logs = require('../api/logs');
 
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
-app.use(cors());
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+
+}));
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const port = process.env.PORT || 1300;
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
